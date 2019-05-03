6b92c646fd4dabec67608eb6178354575e0389cd
krushndayshmookh <krushndayshmookh@gmail.com>
2019-05-03 09:04:56 +0530

    ren people->person

---
+++ models/person.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var PersonData = new Schema({
+	name: {
+		type: String,
+		required: true,
+		max: 100,
+		default: 'unknown'
+	},
+	position: {
+		type: String,
+		max: 100,
+		default: 'unknown'
+	},
+	education: {
+		type: String,
+		required: true,
+		max: 100,
+		default: 0
+	},
+	thought: {
+		type: String,
+		max: 1000,
+		required: true
+	},
+	imageLink: {
+		type: String,
+		max: 1000
+	}
+})
+
+//Export model
+module.exports = mongoose.model(' ', PersonData)
