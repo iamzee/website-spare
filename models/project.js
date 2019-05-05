06ad2f05ec21ccfc94dcf57e6497bb7cbfa3d1e2
krushndayshmookh <krushndayshmookh@gmail.com>
2019-05-05 18:02:16 +0530

    change model to include youtube link, make fields text and add images links

--- models/project.js
+++ models/project.js
@@ -1,66 +1,71 @@
-var mongoose = require('mongoose');
-var moment = require('moment');
+var mongoose = require('mongoose')
+var moment = require('moment')
 
-var Schema = mongoose.Schema;
+var Schema = mongoose.Schema
 
 var ProjectSchema = new Schema({
-    name: {
-        type: String,
-        //required: true,
-        max: 100,
-        default: "unknown"
-    },
-    owner: {
-        type: String,
-        //required: true,
-        max: 100,
-        default: "unknown"
-    },
-    description: {
-        type: String,
-        max: 1000,
-        default: "unknown"
-    },
-    date: {
-        type: Date,
-        //required: true,
-        default: new Date()
-    },
-    cost: {
-        type: Number,
-        //required: true,
-        min: 0,
-        default: 0
-    },
-    url: {
-        type: String,
-        default: ""
-    },
-    categories: [{
-        type: String,
-        max: 100
-    }],
-    image: {
-        data: {
-            type: Buffer,
-        },
-        contentType: {
-            type: String,
-            max: 15,
-            default: 'png'
-        }
-    }
-});
+	name: {
+		type: String,
+		//required: true,
+		max: 100,
+		default: ''
+	},
+	owner: {
+		type: String,
+		//required: true,
+		max: 100,
+		default: ''
+	},
+	description: {
+		type: String,
+		max: 1000,
+		default: ''
+	},
+	date: {
+		type: String,
+		//required: true,
+		default: ''
+	},
+	cost: {
+		type: String,
+		//required: true,
 
+		default: ''
+	},
+	projectUrl: {
+		type: String,
+		default: ''
+	},
+	youtubeUrl: {
+		type: String,
+		default: ''
+	},
+	categories: [
+		{
+			type: String,
+			max: 100
+		}
+	],
+	image: {
+		data: {
+			type: Buffer
+		},
+		contentType: {
+			type: String,
+			max: 15,
+			default: 'png'
+		}
+	},
+	images: [
+		{
+			type: String
+		}
+	]
+})
 
-ProjectSchema
-    .virtual('dateformatted')
-    .get(function () {
-        return moment(this.date).format('MMMM Do, YYYY');
-    });
-
-
+ProjectSchema.virtual('dateformatted').get(function() {
+	return moment(this.date).format('MMMM Do, YYYY')
+})
 
 //Export model
-module.exports = mongoose.model('Project', ProjectSchema);
-
+module.exports = mongoose.model('Project', ProjectSchema)
