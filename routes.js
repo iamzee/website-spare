33f587ea89e11ec0638dc6ae10f657382500388f
sahilkindarle <sahilkindarle212@gmail.com>
2019-05-14 12:21:38 +0530

    route updated

--- routes.js
+++ routes.js
@@ -1,33 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
 
 // User side pages -----
 
-router.get('/', function (req, res) {
-    res.render('index');
+router.get("/", function(req, res) {
+  res.render("index");
 });
 
-router.get('/contact', function (req, res) {
-    res.render('contact',  {
-        status: false
-    });
+router.get("/contact", function(req, res) {
+  res.render("contact", {
+    status: false
+  });
 });
 
 // router.get('/about', function (req, res) {
 //     res.render('about');
 // });
 
-router.get('/support', function (req, res) {
-    res.render('support');
+router.get("/support", function(req, res) {
+  res.render("support");
 });
 
-router.get('/privacypolicy', function (req, res) {
-    res.render('privacypolicy');
+router.get("/privacypolicy", function(req, res) {
+  res.render("privacypolicy");
 });
 
-router.get('/logout', function (req, res) {
-    res.redirect('/');
+router.get("/logout", function(req, res) {
+  res.redirect("/");
 });
 /* implemented in project routes -----
 router.get('/gallery', function (req, res) {
@@ -44,139 +43,134 @@ router.get('/shop', function (req, res) {
 
 // Admin routes -----
 
-router.get('/login', function (req, res) {
-    res.render('login');
+router.get("/login", function(req, res) {
+  res.render("login");
 });
 
-router.post('/login', function (req, res) {
-    //- console.log(req.body);
-    if (req.body.username == 'sagar' && req.body.password == 'sparc') {
-        res.redirect('/dashboard');
-    } else {
-        res.render('login', {
-            failure: true
-        });
-    }
-
+router.post("/login", function(req, res) {
+  //- console.log(req.body);
+  if (req.body.username == "sagar" && req.body.password == "sparc") {
+    res.redirect("/dashboard");
+  } else {
+    res.render("login", {
+      failure: true
+    });
+  }
 });
 
 /*router.get('/dashboard', function (req, res) {
     res.render('dashboard');
 });*/
 
-
-
 // Data Routes -----
 
 // var async = require('async')
 
 // Require controller modules.
-var enquiry_controller = require('./controllers/enquiryController');
-var product_controller = require('./controllers/productController');
+var enquiry_controller = require("./controllers/enquiryController");
+var product_controller = require("./controllers/productController");
 // var productcategory_controller = require('./controllers/productcategoryController');
-var project_controller = require('./controllers/projectController');
-var person_controller = require('./controllers/personController');
+var project_controller = require("./controllers/projectController");
+var person_controller = require("./controllers/personController");
 
-
-router.get('/person/list', person_controller.person_list);
-router.post('/person/create', person_controller.person_create);
+router.get("/person/list", person_controller.person_list);
+router.post("/person/create", person_controller.person_create);
 
 //
-router.get('/person/create', function(req,res) {
-    res.render('edit-about');
+router.get("/person/create", function(req, res) {
+  res.render("edit-about");
 });
 
 //Delete Person
-router.post('/person/:id/delete', person_controller.person_delete_post);
+router.post("/person/delete/:id", person_controller.person_delete_post);
 
 //delete ll team
-router.get('/person/delete/all/team',person_controller.person_delete_all_get);
+router.get("/person/delete/all/team", person_controller.person_delete_all_get);
 
-
-router.get('/about', person_controller.about_get);
+router.get("/about", person_controller.about_get);
 
 /// ENQUIRY ROUTES ///
 
-
-
 // POST request for creating Enquiry.
-router.post('/enquiry/create', enquiry_controller.enquiry_create_post);
+router.post("/enquiry/create", enquiry_controller.enquiry_create_post);
 
-router.post('/enquiry/contact', enquiry_controller.enquiry_contact_create_post);
+router.post("/enquiry/contact", enquiry_controller.enquiry_contact_create_post);
 
 // POST request to delete Enquiry.
-router.post('/enquiry/:id/delete', enquiry_controller.enquiry_delete_get);
+router.post("/enquiry/:id/delete", enquiry_controller.enquiry_delete_get);
 
 // GET request for one Enquiry.
-router.get('/enquiry/:id', enquiry_controller.enquiry_detail);
+router.get("/enquiry/:id", enquiry_controller.enquiry_detail);
 
 // GET request for list of all Enquiry items.
-router.get('/dashboard/enquiries', enquiry_controller.enquiry_list);
+router.get("/dashboard/enquiries", enquiry_controller.enquiry_list);
 
 // GET request for list of all unread Enquiry items.
-router.get('/dashboard', enquiry_controller.dashboard_list);
-
+router.get("/dashboard", enquiry_controller.dashboard_list);
 
 /// PRODUCT ROUTES ///
 
-
 // GET request for creating product.
-router.get('/product/create', function (req, res) {
-    res.redirect('/dashboard/products');
+router.get("/product/create", function(req, res) {
+  res.redirect("/dashboard/products");
 });
 
 // POST request for creating product.
-router.post('/product/create', product_controller.product_create_post);
+router.post("/product/create", product_controller.product_create_post);
 
 // POST request to delete product.
-router.post('/product/:id/delete', product_controller.product_delete_post);
+router.post("/product/:id/delete", product_controller.product_delete_post);
 
 // POST request to update product.
-router.post('/product/:id/update', product_controller.product_update_post);
+router.post("/product/:id/update", product_controller.product_update_post);
 
 // GET request for one product.
-router.get('/product/:id', product_controller.product_detail);
+router.get("/product/:id", product_controller.product_detail);
 
 // GET request for list of all products.
-router.get('/dashboard/products', product_controller.product_edit);
+router.get("/dashboard/products", product_controller.product_edit);
 
-router.get('/shop', product_controller.product_list);
+router.get("/shop", product_controller.product_list);
 
 // GET request for one product image.
-router.get('/product/image/:id', product_controller.product_image_get);
-
-
+router.get("/product/image/:id", product_controller.product_image_get);
 
 /// PROJECT ROUTES ///
 
 // GET request for creating project.
-router.get('/project/create', project_controller.project_edit);
+router.get("/project/create", project_controller.project_edit);
 
 // POST request for creating project.
-router.post('/project/create', project_controller.project_create_post);
+router.post("/project/create", project_controller.project_create_post);
 
 // POST request to delete project.
-router.post('/project/:id/delete', project_controller.project_delete_post);
+router.post("/project/:id/delete", project_controller.project_delete_post);
 
 // POST request to update project.
-router.post('/project/:id/update', project_controller.project_update_post);
+router.post("/project/:id/update", project_controller.project_update_post);
 
 // GET request for one project.
-router.get('/project/:id', project_controller.project_detail);
+router.get("/project/:id", project_controller.project_detail);
 
 // GET request for list of all project.
-router.get('/dashboard/projects', project_controller.project_edit);
+router.get("/dashboard/projects", project_controller.project_edit);
 
-router.get('/projects', project_controller.project_list);
+router.get("/projects", project_controller.project_list);
 
 // GET request for one project image.
-router.get('/project/image/:id', project_controller.project_image_get);
+router.get("/project/image/:id", project_controller.project_image_get);
 
-router.get('/api/projects', project_controller.project_list_api);
+router.get("/api/projects", project_controller.project_list_api);
 
-router.get('/api/project/sign-s3/put', project_controller.project_sign_s3_put_get)
+router.get(
+  "/api/project/sign-s3/put",
+  project_controller.project_sign_s3_put_get
+);
 
-router.get('/api/project/image/delete', project_controller.project_s3_delete_get)
+router.get(
+  "/api/project/image/delete",
+  project_controller.project_s3_delete_get
+);
 
 /* Will be implemented in future if needed ---- *
 /// CATEGORY ROUTES ///
@@ -206,10 +200,6 @@ router.get('/productcategory/:id', productcategory_controller.productcategory_de
 router.get('/productcategories', productcategory_controller.productcategory_list);
 
 /* */
-
-
-
-
 
 //export this router to use in our index.js
 module.exports = router;
